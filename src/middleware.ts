cb39a6a2d2d5d211d3a6f73e215085c7a971ef8d
ahmadfrazz <ahmedfaraz12580@gmail.com>
2024-12-19 20:43:49 +0500

    stripe public

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,5 +20,6 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/stripe/:path*'],
+  // matcher: ['/dashboard/:path*', '/stripe/:path*'],
+  matcher: ['/dashboard/:path*'],
 };
